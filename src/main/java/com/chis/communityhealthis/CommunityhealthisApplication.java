9c6727f86c5f0a6904cf51e508d3d75e9b4131af
BRYAN TAN JIA QIN <49793509+bryantan99@users.noreply.github.com>
2021-05-18 17:12:52 +0800

    Initial setup

---
+++ src/main/java/com/chis/communityhealthis/CommunityhealthisApplication.java
@@ -0,0 +1,13 @@
+package com.chis.communityhealthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityhealthisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityhealthisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chis/communityhealthis/CommunityhealthisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chis.communityhealthis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityhealthisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
