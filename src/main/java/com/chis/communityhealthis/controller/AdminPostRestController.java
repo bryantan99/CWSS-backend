77d6c531c31d89504d542d17629b2576b3861204
bryantan99 <tanjiaqin99@gmail.com>
2021-05-29 19:33:56 +0800

    Rename folder "frontend" to "webapp". Update addPost() to handle uploaded files

--- src/main/java/com/chis/communityhealthis/controller/AdminPostRestController.java
+++ src/main/java/com/chis/communityhealthis/controller/AdminPostRestController.java
@@ -1,12 +1,18 @@
 package com.chis.communityhealthis.controller;
 
+import com.chis.communityhealthis.bean.PostBean;
 import com.chis.communityhealthis.model.PostForm;
 import com.chis.communityhealthis.service.AuthService;
 import com.chis.communityhealthis.service.PostService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/post")
@@ -19,14 +25,16 @@ public class AdminPostRestController {
     private AuthService authService;
 
     @RequestMapping(value = "/get-admin-posts", method = RequestMethod.GET)
-    public ResponseEntity<?> getAdminPosts() {
-        return new ResponseEntity<>(postService.getPosts(), HttpStatus.OK);
+    public ResponseEntity<List<PostBean>> getAdminPosts() {
+        return new ResponseEntity<>(postService.getPostsWithMedia(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/add-admin-post", method = RequestMethod.POST)
-    public ResponseEntity<?> addAdminPosts(@RequestBody PostForm postForm) {
-        postForm.setCreatedBy(authService.getCurrentLoggedInUsername());
-        return new ResponseEntity<>(postService.addPost(postForm), HttpStatus.OK);
+    public ResponseEntity<?> addAdminPosts(@RequestParam(value = "form") String postForm, @RequestParam(value = "files", required = false) List<MultipartFile> multipartFileList) throws IOException {
+        PostForm postFormObj = new ObjectMapper().readValue(postForm, PostForm.class);
+        postFormObj.setCreatedBy(authService.getCurrentLoggedInUsername());
+        postFormObj.setFileList(multipartFileList);
+        return new ResponseEntity<>(postService.addPost(postFormObj), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/delete-admin-post/{postId}", method = RequestMethod.DELETE)
